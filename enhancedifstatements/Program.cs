b701cef3b3c9308fa45ff286f7b19435dcf3908b
Carlos Bautista <cbautista2013@gmail.com>
2021-04-30 00:03:13 -0700

    added more examples

---
+++ enhancedifstatements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace enhancedifstatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = -5;
+            string stateofmatter;
+
+            if (temperature < 0){
+                stateofmatter = "solid";
+            }else{
+                stateofmatter = "liquid";
+            }
+            Console.WriteLine("State of matter is {0}", stateofmatter);
+
+            //in short
+            stateofmatter = temperature < 0 ? "solid":"liquid";
+        }
+    }
+}

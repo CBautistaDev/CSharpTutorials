674ebd33ee0c47d47d9dde3c71e6e8433746dccc
Carlos Bautista <cbautista2013@gmail.com>
2021-05-01 00:00:08 -0700

    added more examples

---
+++ Challenges/loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int counter = 0;
+            string entered = "";
+            bool validint;
+            int finalnumber;
+            int averagescore;
+
+           while (entered != "-1"){
+               Console.WriteLine("Please enter student score or -1 to quit ");
+               entered = Console.ReadLine();
+               validint = int.TryParse(entered, out finalnumber);
+               if (validint && finalnumber > 0 && finalnumber < 20){
+                   total += finalnumber;
+                   counter++;
+                   Console.WriteLine(counter);
+                   Console.WriteLine(total);
+               }else{
+                   Console.WriteLine("not a valid number. Please enter -1 to quit");
+               }
+               
+           }
+
+           averagescore = (total/counter);
+           Console.WriteLine(averagescore);
+        
+            
+        }
+    }
+}

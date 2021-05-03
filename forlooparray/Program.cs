54bcd766be278e4d5873638e7b0f92a8f9934f6c
Carlos Bautista <cbautista2013@gmail.com>
2021-05-03 13:40:28 -0700

    loop example

---
+++ forlooparray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace forlooparray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            for(int i = 0; i < 10; i++){
+                nums[i] = i;
+            }
+
+            for(int j=0; j < 10; j ++){
+                Console.WriteLine("Element{0} = {1}", j , nums[j]);
+            }
+        }
+    }
+}
